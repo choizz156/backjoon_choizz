69fff578e87169f4b45951b1cd07766904f43b78
최민석 <106965005+choizz156@users.noreply.github.com>
2025-06-20 14:00:32 +0900

    [Gold V] Title: 보물섬, Time: 456 ms, Memory: 190464 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,89 +1,89 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-    static int n, m, mx;
-    static char[][] a = new char[54][54];
-    static int[][] visit;
+class Main {
+    
+    static int n, m;
+    static int[][] a, v;
+    
     static int[] dy = {-1,0,1,0};
-    static int[] dx = {0,1,0,-1}; 
-
-
+    static int[] dx = {0,1,0,-1};
+   
     public static void main(String[] args) throws Exception{
-       Scan sc = new Scan();
-       n = sc.nextInt();
-       m = sc.nextInt();
-
-       for(int i = 0; i < n; i++){
-        String s = sc.next();
-        for(int j = 0; j < m; j++){
-            a[i][j] = s.charAt(j);
+      
+        var sc = new Scan();
+        
+        n = sc.nextInt();
+        m = sc.nextInt();
+        a = new int[n][m];
+        
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            for(int j = 0; j < m; j++){
+                a[i][j] = (s.charAt(j) == 'W') ? 0 : 1;
+            }
         }
-       }
-
-        mx = -111111;
+        
+        int max = -1;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                visit = new int[54][54];
-                if(a[i][j] == 'L'){
-                    bfs(i,j);
+                v = new int[n][m];
+                int d = 0;
+                if(v[i][j] == 0 && a[i][j] == 1){
+                    d = bfs(i,j);
                 }
+                max = Math.max(max, d);
             }
         }
-
-        System.out.println(mx - 1);
-
+        
+        System.out.println(max);
     }
-
-    static void bfs(int y, int x){
-        visit[y][x] = 1;
-
+    
+    static int bfs(int y, int x){
+        int k = 0;
         Deque<int[]> q = new ArrayDeque<>();
-        q.addLast(new int[]{y,x});
-
+        q.add(new int[]{y,x});
+        v[y][x] = 1;
+        
         while(!q.isEmpty()){
-            int[] f = q.pollFirst();
-            y = f[0];
-            x = f[1];
-
+            
+            int[] f = q.poll();
+            int fy = f[0];
+            int fx = f[1];
+            
             for(int i = 0; i < 4; i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-
-                if(nx < 0 || ny < 0 || nx >= m || ny >= n || visit[ny][nx] != 0 || a[ny][nx] == 'W') 
-                    continue;
-                visit[ny][nx] = visit[y][x] + 1;
-                q.addLast(new int[]{ny, nx});
-                mx = Math.max(mx, visit[ny][nx]);
+                int ny = fy + dy[i];
+                int nx = fx + dx[i];
+                
+                if(ny < 0 || nx < 0 || nx >= m || ny >= n || v[ny][nx] != 0 || a[ny][nx] == 0) continue;
+                v[ny][nx] = v[fy][fx] + 1;
+                k = v[ny][nx];
+                q.add(new int[]{ny,nx});                
             }
         }
-
+        
+        return k - 1;
     }
-
-
 
     static class Scan{
         BufferedReader br;
         StringTokenizer st;
-
-        Scan(){
+        
+        public Scan(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-
+        
         String next() throws Exception{
             while(st == null || !st.hasMoreElements()){
                 st = new StringTokenizer(br.readLine());
             }
             return st.nextToken();
         }
-
+        
         int nextInt() throws Exception{
             return Integer.parseInt(next());
         }
-
-        String nextLine() throws Exception{
-            return br.readLine();
-        }
+        
+        
     }
 }
