a3a736b7dfa79d8bdc3ff275feee23f6d6ec3b8b
최민석 <106965005+choizz156@users.noreply.github.com>
2025-08-25 13:23:33 +0900

    [level 1] Title: 모의고사, Time: 5.94 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,33 +1,34 @@
 import java.util.*;
 
 class Solution {
-     int retA, retB, retC;
-	 final int[][] a = {
-        {-1},
+	int[][] a = {
+        {},
         {1, 2, 3, 4, 5},
         {2, 1, 2, 3, 2, 4, 2, 5},
         {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
     };
-    
     public int[] solution(int[] ans) {
-        	var cnt = new int[4];
-            
-            for(int i = 0; i < ans.length; i++){
-                for(int j = 1; j < a.length; j++){
-                    if(ans[i] == a[j][i % a[j].length]){
-                        cnt[j]++;
-                    }
+        var cnt = new int[4];
+       	int len = ans.length;
+      	
+        for(int i = 0; i < len; i++){
+            for(int j = 1; j < a.length; j++){
+                if(ans[i] == a[j][i % a[j].length]){
+                    cnt[j]++;
                 }
             }
+        }
         
-        	int max = Arrays.stream(cnt).max().getAsInt();
-        	var ret = new ArrayList<Integer>();
-        	for(int i = 1; i < cnt.length; i++){
-                if(cnt[i] == max){
-                    ret.add(i);
-                }
-            }
-        
-        return ret.stream().mapToInt(Integer::intValue).toArray();
+        int max = Arrays.stream(cnt).max().getAsInt();
+		var l = new ArrayList<Integer>();
+		int idx = 0;
+        for(int v : cnt){
+           if(v == max) {
+               l.add(idx); 
+           }
+            idx++;
+        }
+       	
+       return l.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
