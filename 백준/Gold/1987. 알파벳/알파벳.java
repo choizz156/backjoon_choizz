284895be5f573d5ce8b448e816e86bcc3084fadf
최민석 <106965005+choizz156@users.noreply.github.com>
2025-08-26 19:15:39 +0900

    [Gold IV] Title: 알파벳, Time: 860 ms, Memory: 14924 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,64 +1,76 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-   	
-    static int r, c, ret;
-    static char[][] a = new char[24][24];
-    static int[] v = new int[26];
-    static int[] dy = {-1,0,1,0};
-    static int[] dx = {0,1,0,-1};
 
-    public static void main(String[] args) throws Exception {
-        
-        Scan sc = new Scan();
-        r = sc.nextInt();
-        c = sc.nextInt();
-        for(int i = 0; i < r; i++){
-            String s = sc.next();
-            for(int j = 0; j < c; j++){
-                a[i][j] = s.charAt(j);
-            }
-        }
-        
-        v[a[0][0] - 'A'] = 1;
-        go(0,0,1);
-        System.out.println(ret);
-      
-    }
+public class Main {
     
-    static void go(int y, int x, int cnt){
-        ret = Math.max(ret,cnt);
+     static int r, c, ret;
+     static char[][] board;
+     static boolean[] v = new boolean[26];
+     static int[] dy = {-1, 0, 1, 0};
+     static int[] dx = {0,1,0,-1};
+
+
+    public static void main(String[] args) throws Exception{
+        
+      var sc = new Scan();
+      r = sc.nextInt();
+      c = sc.nextInt();
+
+      board = new char[r][c];
+
+      for(int i = 0; i < r; i++){
+        var l = sc.nextLine();
+        board[i] = l.toCharArray();
+      }  
+
+      v[board[0][0] - 'A'] = true;
+      dfs(0,0,1);
+      System.out.println(ret);
+    }
+
+    static void dfs(int y, int x, int cnt){
+        ret = Math.max(ret, cnt);
+
         for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= r || nx >=c) continue;
-            int next = a[ny][nx] - 'A';
-            if(v[next] == 0){
-                v[next] = 1;
-                go(ny,nx,cnt + 1);
-                v[next] = 0;
+
+            if (ny < 0 || ny >= r || nx < 0 || nx >= c) continue;
+
+            int idx = board[ny][nx] - 'A';
+
+            if(!v[idx]){
+                v[idx] = true;
+                dfs(ny, nx, cnt + 1);
+                v[idx] = false;
             }
         }
     }
-    
+
+
+
     static class Scan{
         BufferedReader br;
         StringTokenizer st;
-        
+
         Scan(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-        
+
         String next() throws Exception{
             while(st == null || !st.hasMoreElements()){
                 st = new StringTokenizer(br.readLine());
             }
             return st.nextToken();
         }
-        
+
         int nextInt() throws Exception{
             return Integer.parseInt(next());
+        }
+
+        String nextLine() throws Exception{
+            return br.readLine();
         }
     }
 }
