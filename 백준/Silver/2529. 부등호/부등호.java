032d900e9c8951f76f1eb6fd8ec4c2917f197d2a
최민석 <106965005+choizz156@users.noreply.github.com>
2025-08-28 20:24:45 +0900

    [Silver I] Title: 부등호, Time: 220 ms, Memory: 23580 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -3,65 +3,74 @@ import java.util.*;
 
 class Main{
     
-    static int n;
+  static int n;
     static char[] a = new char[10];
-    static boolean check[] = new boolean[10];
+    static boolean[] v = new boolean[10];
     static List<String> ret = new ArrayList<>();
-        
-    public static void main(String[] args) throws Exception {
-        Scan sc = new Scan();
+    
+
+    public static void main(String[] args) throws Exception{
+        var sc = new Scan();
         n = sc.nextInt();
-        for(int i = 0; i < n; i++)
+
+        for(int i = 0; i < n; i++){
             a[i] = sc.next().charAt(0);
-        
+        }
+
         go(0, "");
 
         Collections.sort(ret);
-        System.out.println(ret.get(ret.size() - 1));
-        System.out.println(ret.get(0));
+		System.out.println(ret.get(ret.size()-1));
+		System.out.println(ret.get(0));
     }
-    
+
     static void go(int idx, String num){
         if(idx == n + 1){
             ret.add(num);
             return;
         }
-        
+
         for(int i = 0; i <= 9; i++){
-            if(check[i]) continue;
-            if(idx == 0 || good(num.charAt(idx - 1), (char)(i +  '0'), a[idx - 1])){
-                check[i] = true;
+            if(v[i]) continue;
+            if(idx == 0 || good(num.charAt(idx - 1), (char)(i + '0'), a[idx - 1])){
+                v[i] = true;
                 go(idx + 1, num + i);
-                check[i] = false;
+                v[i] = false;
             }
         }
     }
-    
+
     static boolean good(char x, char y, char op){
         if(x < y && op == '<') return true;
         if(x > y && op == '>') return true;
         return false;
     }
-    
-    
-        
-     static class Scan{
+
+
+
+
+
+    static class Scan{
         BufferedReader br;
         StringTokenizer st;
-        
+
         Scan(){
             br = new BufferedReader(new InputStreamReader(System.in));
         }
-        
+
         String next() throws Exception{
             while(st == null || !st.hasMoreElements()){
                 st = new StringTokenizer(br.readLine());
             }
             return st.nextToken();
         }
-        
+
         int nextInt() throws Exception{
             return Integer.parseInt(next());
+        }
+
+        String nextLine() throws Exception{
+            return br.readLine();
         }
     }
     
