0fc45dfc8492416786f583c341634472cdaf90f8
최민석 <106965005+choizz156@users.noreply.github.com>
2025-08-28 20:10:35 +0900

    [level 2] Title: 방문 길이, Time: 20.30 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,36 +2,37 @@ import java.util.*;
 
 class Solution {
     
-
-    static Map<Character, int[]> location = new HashMap<>();
-    
-    static void init(){
-        location.put('U', new int[]{0,1});
-        location.put('D', new int[]{0,-1});
-        location.put('L', new int[]{-1,0});
-        location.put('R', new int[]{1,0});
-    }
+    static Map<Character, int[]> l = new HashMap<>();
     
     public int solution(String dirs) {
-        init();
+    	
+        l.put('U', new int[]{0,1});
+        l.put('D', new int[]{0,-1});
+        l.put('L', new int[]{-1,0});
+        l.put('R', new int[]{1,0});
+        
         int x = 5, y = 5;
         var ans = new HashSet<String>();
         
         int len = dirs.length();
         for(int i = 0; i < len; i++){
-        	var offset = location.get(dirs.charAt(i));
-            var nx = x + offset[0];
-            var ny = y + offset[1];
+            var f = l.get(dirs.charAt(i));
+            var nx = x + f[0];
+            var ny = y + f[1];
             
             if(0 <= nx && nx < 11 && 0 <= ny && ny < 11){
-                ans.add(x+""+ y+ "" + nx + "" + ny);
-                ans.add(nx+""+ny+""+x+""+y);
+                ans.add(""+ x + y + nx + ny);
+                ans.add(""+ nx + ny + x + y);
+                
                 x = nx;
                 y = ny;
             }
         }
         
-        return ans.size()/2;
-        
+        return ans.size() / 2;
+       
     }
+        
+       
+        
 }
